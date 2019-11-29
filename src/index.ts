87c47b1134fe15f900eda13da07fb0bb5a9ed81e
nicolas <nicolas.hoog@gmail.com>
2019-11-29 15:51:34 -0500

    add trigger method for event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('cfdfs', () => {});
+user.on('change', () => {
+  console.log('1');
+});
+user.on('change', () => {
+  console.log('2');
+});
+user.on('save', () => {
+  console.log('save');
+});
 
-console.log(user);
+user.trigger('save');
